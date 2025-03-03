ead103b87df22a4764ae35487515edaf2248740b
livepaxx <71604539+livepaxx@users.noreply.github.com>
2025-03-03 11:10:25 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,18 +1,18 @@
 let userId = "user123"; // Replace with Telegram Web Apps API user_id
 
 async function clickButton() {
-    const response = await fetch(`/click/?user_id=${userId}`, { method: "POST" });
+    const response = await fetch(`https://backend-iqp0.onrender.com/click/?user_id=${userId}`, { method: "POST" });
     const data = await response.json();
     document.getElementById("score").innerText = `Your Score: ${data.score}`;
 }
 
 async function resetScore() {
-    await fetch(`/reset/?user_id=${userId}`, { method: "POST" });
+    await fetch(`https://backend-iqp0.onrender.com/reset/?user_id=${userId}`, { method: "POST" });
     document.getElementById("score").innerText = "Your Score: 0";
 }
 
 async function getScore() {
-    const response = await fetch(`/score/?user_id=${userId}`);
+    const response = await fetch(`https://backend-iqp0.onrender.com/score/?user_id=${userId}`);
     const data = await response.json();
     document.getElementById("score").innerText = `Your Score: ${data.score}`;
 }
